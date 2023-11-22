82dd038f368f2920c87c0e1402cfe4e8833c65ee
artemis-133 <sidcoding133@gmail.com>
2023-11-22 19:29:35 +0530

    Minor error

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import App from "./App";
 import store from "./redux/store";
 import { BrowserRouter } from "react-router-dom";
 import "remixicon/fonts/remixicon.css";
-import { Provider } from "react-redux";
-import store from "./redux/store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
